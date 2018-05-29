cfa612fa17923c4fe0c46ced6991ae24bfce9ca4
Bartłomiej Hałdaś <haldas.bartlomiej@gmail.com>
2018-05-29 13:30:24 +0200

    Create fileconf.js

---
+++ src/public/fileconf.js
@@ -0,0 +1,10 @@
+var fileconf = {
+    apiKey: "AIzaSyB9Is-rNsWgM-V1EexcJQeq0babWrO_xYk",
+    authDomain: "aktywny-krakow-ae725.firebaseapp.com",
+    databaseURL: "https://aktywny-krakow-ae725.firebaseio.com",
+    projectId: "aktywny-krakow-ae725",
+    storageBucket: "aktywny-krakow-ae725.appspot.com",
+    messagingSenderId: "525000600389"
+  };
+
+  //inicjalizacja firebase
